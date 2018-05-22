a47243976b3c2c19736eeed6c5f218e6bd109955
Cauê Baasch de Souza <cauebs@pm.me>
2018-05-22 02:27:51 -0300

    Slightly improve finite_automaton; implement copying

--- kleeneup/finite_automaton.py
+++ kleeneup/finite_automaton.py
@@ -1,16 +1,11 @@
 from collections import defaultdict
 from string import ascii_lowercase, digits
-from typing import DefaultDict, Iterable, Set, Iterator, Union
+from typing import DefaultDict, Iterable, Set, Iterator, Union, Dict
 
 
 class Symbol:
     def __init__(self, value: str) -> None:
-        if (
-            value != '&'
-            and value not in ascii_lowercase
-            and value not in digits
-            or len(value) != 1
-        ):
+        if len(value) != 1 or value not in ascii_lowercase + digits + '&':
             raise ValueError('Symbol must be a lowercase letter or a digit')
         self.value = value
 
@@ -137,6 +132,33 @@ class FiniteAutomaton:
             raise ValueError('Argument accept_states must be subset of states')
         self.accept_states = accept_states
 
+    def copy(self) -> 'FiniteAutomaton':
+        state_swap: Dict[State, State] = {
+            state: State(state.name) for state in self.states
+        }
+
+        for state in self.states:
+            for symbol, next_state_overlap in state.transitions.items():
+                for next_state in next_state_overlap:
+                    state_swap[state][symbol] = state_swap[next_state]
+
+        return FiniteAutomaton(
+            set(state_swap.values()),
+            state_swap[self.initial_state],
+            {state_swap[state] for state in self.accept_states},
+        )
+
+    def prefix_state_names(self, prefix):
+        for state in self.states:
+            state.name = f'{prefix}{state.name}'
+
+    def reset_state_names(self):
+        self.initial_state.name = 'Q0'
+        for i, state in enumerate(self.states):
+            if state is self.initial_state:
+                continue
+            state.name = f'Q{i + 1}'
+
 #     @classmethod
 #     def from_regular_grammar(cls, grammar):
 #         ...
@@ -154,11 +176,14 @@ class FiniteAutomaton:
 #     def minimize(self):
 #         ...
 
-#     def evaluate(self, string):
-#         ...
+    def evaluate(self, sentence: Sentence):
+        return any(
+            state in self.initial_state[sentence]
+            for state in self.accept_states
+        )
 
     def complete(self):
-        error_state = State('_Qerror')
+        error_state = State('Qerror')
 
         for state in self.states:
             for symbol in self.alphabet:
@@ -167,8 +192,8 @@ class FiniteAutomaton:
 
         self.states.add(error_state)
 
-#     def reverse(self):
-#         ...
+    # def reverse(self):
+    #     ...
 
 #     def kleene_star(self):
 #         ...
@@ -184,7 +209,7 @@ class FiniteAutomaton:
 #         ...
 
     def union(self, other: 'FiniteAutomaton') -> 'FiniteAutomaton':
-        new_initial_state = State('_q0')
+        new_initial_state = State('Q0')
 
         for symbol, states in self.initial_state.transitions.items():
             for state in states:
@@ -195,13 +220,15 @@ class FiniteAutomaton:
                 new_initial_state[symbol] = state
 
         new_states = set.union(self.states, other.states)
+        new_states.add(new_initial_state)
+
         new_accept_states = set.union(self.accept_states, other.accept_states)
 
         return FiniteAutomaton(
             new_states,
             new_initial_state,
             new_accept_states,
-        )
+        ).copy()
 
     def concatenate(self, other: 'FiniteAutomaton') -> 'FiniteAutomaton':
         new_initial_state = self.initial_state
@@ -212,10 +239,12 @@ class FiniteAutomaton:
                     accept_state[symbol] = state
 
         new_states = set.union(self.states, other.states)
+        new_states.add(new_initial_state)
+
         new_accept_states = other.accept_states
 
         return FiniteAutomaton(
             new_states,
             new_initial_state,
             new_accept_states,
-        )
+        ).copy()
