6d895b4f084d496763dcbd6f3d51a8e91f2d3350
Cauê Baasch de Souza <cauebs@pm.me>
2018-10-11 01:28:56 -0300

    Fix regular grammar parsing test

--- tests/test_regular_grammar.py
+++ tests/test_regular_grammar.py
@@ -2,13 +2,13 @@ from kleeneup import RegularGrammar
 
 
 def test_from_and_to_string():
-    rules = (
-        "S' -> aS | &"
-        "S -> aA"
-        "A -> aA | bB | a"
-        "B -> b"
-    )
+    rules = '''
+        S' -> aS | &
+        S -> aA
+        A -> aA | bB | a
+        B -> b
+    '''
     rg = RegularGrammar.from_string(rules)
+    s = str(rg)
 
-    s = rg.to_string()
-    assert RegularGrammar.from_string(s).to_string() == s
+    assert str(RegularGrammar.from_string(s)) == s
